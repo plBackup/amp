c6b3ac732cf28f96260626c654bc4d89d0354ccb
李楣庭 <limeiting@limeitingdeMacBook-Pro.local>
2016-11-08 22:53:02 +0800

    all

--- app/static/js/main/amp_ag.js
+++ app/static/js/main/amp_ag.js
@@ -6,29 +6,52 @@
 /* App Module */
 
 var ampApp = angular.module('amp', [
-    'ngRoute',
+    'ui.router',
     'ampControllers',
-    'ampFilters',
-    'ampServices',
-     'ng-swipe',
+    'ampFilters'
 ]);
 
-ampApp.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.
-        when('/noi', {
-            templateUrl: '../views/noi_analyse/noi.html',
-            //controller: 'ampNOICtrl'
-        }).
-        when('/list', {
-            templateUrl: '../views/noi_analyse/phone-list.html',
-            controller: 'ampListCtrl'
-        }).
-        when('/detail', {
-            templateUrl: '../views/noi_analyse/phone-detail.html',
-            controller: 'ampDetailCtrl'
-        }).
-        otherwise({
-            //redirectTo: '/noi'
+
+ampApp.config(function($stateProvider,$urlRouterProvider) {
+    // An array of state definitions
+    var states = [
+        {
+            name: 'noi',
+            url: '/noi',
+            views:{
+                'toolbar': {
+                    templateUrl: '../components/toolbar/noi_filter.html',
+                },
+                'content': {
+                    templateUrl: '../views/noi_analyse/noi.html',
+                },
+            },
+        }, //state
+        {
+            name: 'rpgindex',
+            url: '/rpgindex',
+            views:{
+                'toolbar': {
+                    templateUrl: '../components/toolbar/rent_package_filter.html',
+                },
+                'content': {
+                    templateUrl: '../views/datatools/datatool_rent_package.html',
+                },
+                "right":{
+                    templateUrl: '../views/datatools/datatool_rent_package_rpanel.html',
+                }
+            },
+        }, //state
+    ];
+
+    // Loop over the state definitions and register them
+    states.forEach(function(state) {
+        $stateProvider.state(state);
+    });
+    $urlRouterProvider.when('', '/rpgindex');
+    $urlRouterProvider.when('datatool', '/');
+    $urlRouterProvider.otherwise(
+        function($injector, $location) {
+            $location.path('/');
         });
-    }]);
+});
--- app/static/js/views/noi_analyse/noi.js
+++ app/static/js/views/noi_analyse/noi.js
@@ -1,10 +1,115 @@
 /**
  * Created by limeiting on 16/11/6.
  */
+console.log(".....");
 var noi=(function($,noi){
     var noi=noi;
-
     var myLineChart;
+    var pin;
+    noi.swiper_init=function(){
+        var noi_head_swiper = new Swiper('#noi-main-table-head', {
+            //scrollbar: '.swiper-scrollbar',
+            direction: 'horizontal',
+            slidesPerView: 'auto',
+            //mousewheelControl: true,
+            freeMode: true,
+            scrollbarHide:true
+        });
+        var noi_main_swiper = new Swiper('#noi-main-table', {
+            scrollbar: '.swiper-scrollbar',
+            direction: 'horizontal',
+            slidesPerView: 'auto',
+            //mousewheelControl: true,
+            freeMode: true,
+            scrollbarHide:false
+        });
+        noi_head_swiper.params.control = noi_main_swiper;
+        noi_main_swiper.params.control = noi_head_swiper;
+
+         pin=$(".ys-table-fixed-top").pin({
+            containerSelector: "#noi-main-table-wrapper",
+            padding: {top: 44, bottom: 50}
+        });
+
+        var defer=null;
+        var swiperUpdate=function(){
+            noi_head_swiper.update();
+            noi_main_swiper.update();
+            pin.refresh();
+        };
+
+        $(window).resize(function(){
+            if(!defer){
+
+                defer=setTimeout(function(){
+                    swiperUpdate();
+                    defer=null;
+                },200);
+            }else{
+                clearTimeout(defer);
+                defer=setTimeout(function(){
+                    swiperUpdate();
+                    defer=null;
+                },200);
+            }
+
+        });
+    };
+
+    noi.table_init=function(){
+
+        $(".ys-table-main").on("mouseleave","tr",function(e){
+            var index=$(this).index();
+            var parentTagName=$(this).parent().get(0).tagName;
+            $(this).closest(".ys-table-main").find(".amp-table >"+parentTagName).each(function(i,e){
+                $(this).find("tr").eq(index).removeClass("hover");
+            });
+
+        });
+
+        $(".ys-table-main").on("click","tbody>tr",function(e){
+            if($(this).hasClass("tr-main")){
+                var index=$(this).index();
+                $(this).closest(".ys-table-main").find(".amp-table").each(function(i,e){
+                    var $trMain=$(this).find("tr").eq(index);
+                    $trMain.toggleClass("tr-collapse");
+                    if($trMain.hasClass("tr-collapse")){
+                        $trMain.nextUntil(".tr-main","tr").addClass("tr-collapsed");
+
+                    }else{
+                        //展开时，要判断 tr-sub-main的状态，来更改tr-tri的折叠状态
+                        $trMain.nextUntil(".tr-main","tr.tr-sub").removeClass("tr-collapsed");
+                        var sub_main_collapse= false;
+                        $trMain.nextUntil(".tr-main","tr.tr-tri").each(function(i,e){
+                            if(i==0){
+
+                                sub_main_collapse=$(this).prev(".tr-sub-main").hasClass("tr-collapse");
+                            }
+
+                            if(!sub_main_collapse){
+                                $(this).removeClass("tr-collapsed");
+                            }
+                        });
+                    }
+                });
+                pin.refresh();
+            }
+
+            if($(this).hasClass("tr-sub-main")){
+                var index=$(this).index();
+                $(this).closest(".ys-table-main").find(".amp-table").each(function(i,e){
+                    $(this).find("tr").eq(index).toggleClass("tr-collapse").nextUntil(".tr-sub").toggleClass("tr-collapsed");
+                });
+            }
+        });
+
+        $("#noi-arrearage").on("click","tr.tr-main",function(e){
+            $(this).toggleClass("tr-collapse").nextAll("tr.tr-sub").toggle();
+        });
+
+        //初始化折叠项目
+        $(".tr-init-collapse").trigger("click");
+    };
 
     var chart_data_init={
         lineData:{
@@ -320,6 +425,8 @@ var noi=(function($,noi){
     };
     noi.init=function(){
         noi.chart_init();
+        noi.swiper_init();
+        noi.table_init();
         $('#preloader').delay(350).fadeOut(function(){
             //start
         });
@@ -331,8 +438,10 @@ var noi=(function($,noi){
     return noi;
 })(jQuery,noi||{});
 
-$(document).ready(function(){
-   noi.init();
+
+jQuery(document).ready(function(){
+    alert("...");
+    noi.init();
 });
 
 
