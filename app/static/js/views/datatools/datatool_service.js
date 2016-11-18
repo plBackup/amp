096235489bcdeeaf6edb1ad9b079ecee90767277
李楣庭 <limeiting@limeitingdeMacBook-Pro.local>
2016-11-19 02:09:09 +0800

    update all

--- app/static/js/views/datatools/datatool_service.js
+++ app/static/js/views/datatools/datatool_service.js
@@ -11,7 +11,48 @@ angular.module('dataTool').service('dataIndexService', ["$http",function($http) 
             });
         },
 
+        getPageData:function(){
+
+        }
+
     };
+    return service;
+
+}]);
+
+angular.module('dataTool').service('paginator', [function() {
+    var service = {
+      hasNextVar:false,
+        next:function(){
+           if(this.hasNextVar){
+               this.currentOffset+=pageSize;
+               this._load();
+           }
+        },
+        _load:function(){
+            var self=this;
+            fetchData(this.currentOffset,pageSize+1,function(items){
+                self.currentPageItems=items.slice(0,pageSize);
+                self.hasNextVar= items.length===pageSize+1;
+            });
+        },
+        hasNext:function(){
+            return this.hasNextVar;
+        },
+        previous:function(){
+            if(this.hasPrevious){
+                this.currentOffset ==pageSize;
+                this._load();
+            }
+        },
+        hasPrevious:function(){
+            return this.currentOffset !==0;
+        },
+        currentPageItems:[],
+        currentOffset:0
+    };
+    service._load();
+
     return service;
 
 }]);
