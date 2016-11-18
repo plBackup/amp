7b06a1a16a7017a048ce41f2e6d86cb55e52df49
李楣庭 <limeiting@limeitingdeMacBook-Pro.local>
2016-11-19 01:32:26 +0800

    datatool update

--- app/static/js/views/datatools/datatool_controller.js
+++ app/static/js/views/datatools/datatool_controller.js
@@ -5,7 +5,8 @@ var dataTool=angular.module("dataTool",[]);
 dataTool.controller("dataIndexController",['$rootScope', '$scope',"dataIndexData",
     function($rootScope, $scope,dataIndexData) {
         var self=this;
-        console.log(dataIndexData);
+        self.indexData=dataIndexData.slice(1);
+        console.log(self.indexData);
     }]);
 
 dataTool.controller("dataRightController",['$rootScope', '$scope',
