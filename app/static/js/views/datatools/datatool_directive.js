0e19a32e4e258d45ab427a18d6d352d0aa2d6289
李楣庭 <limeiting@limeitingdeMacBook-Pro.local>
2016-12-05 23:27:47 +0800

    update all

--- app/static/js/views/datatools/datatool_directive.js
+++ app/static/js/views/datatools/datatool_directive.js
@@ -125,3 +125,35 @@ angular.module('dataTool').directive('datePicker', [
             }//end link
         };
     }]);
+
+angular.module('dataTool').directive('simChart', ["chartOptService",
+    function(chartOptService) {
+        return {
+            restrict: 'A',
+            scope: {
+                chartData: '=',
+                chartLabels: '='
+            },
+            link: function($scope, $element) {
+                //console.log("-------ddd")
+                //console.log($element);
+                //var lineChart;
+                var dataSimChart = echarts.init($element[0]);
+
+                var labels = $scope.chartData[0];
+                var data=$scope.chartData.slice(1);
+                var opt=chartOptService.setChartOption(data,labels);
+                dataSimChart.setOption(opt);
+
+                $scope.$watch('chartData', function(newVal, oldVal) {
+                    //console.log(newVal);
+                    if (newVal) {
+                        var labels = $scope.chartData[0];
+                        var data=$scope.chartData.slice(1);
+                        var opt=chartOptService.setChartOption(data,labels);
+                        dataSimChart.setOption(opt);
+                    }
+                });
+            }
+        };
+    }]);
